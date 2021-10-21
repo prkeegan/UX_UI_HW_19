24ff38207d1006ccc2081f7ee66903131a4b67b1
prkeegan <83726682+prkeegan@users.noreply.github.com>
2021-10-20 22:32:21 -0700

    JS

---
+++ js/index.js
@@ -0,0 +1,15 @@
+ScrollReveal({ duration: 1000 })
+
+ScrollReveal().reveal('.navigation', { delay: 500 });
+
+ScrollReveal().reveal('.heroImg', { delay: 500 });
+
+ScrollReveal().reveal('.mySkills', { delay: 500 });
+
+ScrollReveal().reveal('.flexContainer', { delay: 500 });
+
+ScrollReveal().reveal('.workGrid', { delay: 500 });
+
+ScrollReveal().reveal('.work', { delay: 500 });
+
+ScrollReveal().reveal('.footer', { delay: 500 });
